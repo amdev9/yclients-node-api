560de398bbea09181be7e33c4e95703bd6d388d3
Alex Matveev <amatveevdev9@gmail.com>
2021-03-29 14:55:42 +0300

    use correct fetch && all endpoints migration

---
+++ index2.js
@@ -0,0 +1,17 @@
+
+const fetch = require('node-fetch');
+
+
+
+(async function () {
+  const body = {a: 1};
+
+  const response = await fetch('https://httpbin.org/post', {
+    method: 'post',
+    body: JSON.stringify(body),
+    headers: {'Content-Type': 'application/json'}
+  });
+  const data = await response.json();
+  
+  console.log(data);
+})();
